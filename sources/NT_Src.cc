232cf4bfa809236f639954b713aaede319f1a38b
AJ Biffl <ajbiffl219@gmail.com>
2023-09-05 10:57:29 -0600

    setting E1 and E2 beyond Eg[0] so that groups after the eof are skipped

--- sources/NT_Src.cc
+++ sources/NT_Src.cc
@@ -149,10 +149,11 @@ int main(int argc, char *argv[]) {
         if (E2 < gmax) { // read in next values
           E1 = E2;
           s1 = s2;
-          sourcefile >> E2;
-          if (!(sourcefile >> s2)) {
+          if (!(sourcefile >> E2 >> s2)) {
             E_eval.push_back(E1);
             S_eval.push_back(s1);
+            E1 = Eg[0] + 1.;
+            E2 = Eg[0] + 1.;
             loop_again = false;
           }
         } else {
